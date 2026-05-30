73610ef3807bc40f868232d4e9a82fae958ab8cd
mukhut <mukhutjp@gmail.com>
2026-05-30 22:47:02 +0100

    modif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import { ToastContainer } from "react-toastify";
 import "./globals.css";
 import ThemeProvider from "@/components/providers/theme-provider";
 
+export const metadata = {
+  title: "Control Manager",
+  icons: {
+    icon: "/aba.png",
+    shortcut: "/aba.png",
+    apple: "/aba.png",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
